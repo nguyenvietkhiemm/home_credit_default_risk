603fca07851c50c6c966dff36fc8a681d1e23a59
nguyenvietkhiemm <nguyenvietkhiemm@gmail.com>
2025-04-11 15:49:36 +0700

    installments payments features engineering

--- modules/multi.py
+++ modules/multi.py
@@ -2,15 +2,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def multi(c): # diff và pctchange
-    df_grouped = prev.groupby('SK_ID_CURR')[c]
+def multi(c, df):
+    df_grouped = df.groupby('SK_ID_CURR')[c]
 
-    ret_diff = df_grouped.diff() 
-    ret_pctchng = df_grouped.pct_change()  
-    
+    ret_diff = df_grouped.diff()
+
+    prev = df_grouped.shift(1)
+    ret_pctchng = (df[c] - prev) / prev.replace(0, np.nan)
+
     ret_diff = ret_diff.rename(f'{c}_diff')
     ret_pctchng = ret_pctchng.rename(f'{c}_pctchange')
 
-    ret = pd.concat([ret_diff, ret_pctchng], axis=1)
-    
-    return ret
+    return pd.concat([ret_diff, ret_pctchng], axis=1)
+
